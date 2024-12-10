d493514de01ad3083c3d25b1358b6f43c27a9343
Balazs Hetenyi <balazshetenyi82@gmail.com>
2024-12-10 14:32:27 +0000

    added templates and css file

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,26 +5,17 @@ from listings.models import Band, Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django!</h1>
-        <p>My favorite bands are:<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-    """)
+    context = {
+        'bands': bands
+    }
+    
+    return render(request, 'listings/hello.html', context)
 
 
 def about(request):
     listings = Listing.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django!</h1>
-        <p>My favorite listings are:<p>
-        <ul>
-            <li>{listings[0].title}</li>
-            <li>{listings[1].title}</li>
-            <li>{listings[2].title}</li>
-            <li>{listings[3].title}</li>
-        </ul>
-    """)
+    context = {
+        'listings': listings
+    }
+    
+    return render(request, 'listings/about.html', context)
