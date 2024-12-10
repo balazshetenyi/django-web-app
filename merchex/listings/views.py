cbbb8361a12853afe1ed49e59c85e090532c3c08
Balazs Hetenyi <balazshetenyi82@gmail.com>
2024-12-10 11:57:35 +0000

    added new models

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from listings.models import Band, Listing
+
 def hello(request):
-    return HttpResponse('<h1>Hello Django</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django!</h1>
+        <p>My favorite bands are:<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+    """)
 
 
 def about(request):
-    return HttpResponse('<h1>Hello About page!</h1>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django!</h1>
+        <p>My favorite listings are:<p>
+        <ul>
+            <li>{listings[0].title}</li>
+            <li>{listings[1].title}</li>
+            <li>{listings[2].title}</li>
+            <li>{listings[3].title}</li>
+        </ul>
+    """)
